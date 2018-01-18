0fd89754e6a21da5f0ed1ec09e54872404a1d646
seyed <snaba104@uottawa.ca>
2018-01-18 01:32:11 -0500

    another commit :)

--- static/scripts.js
+++ static/scripts.js
@@ -15,13 +15,21 @@ $(document).ready(function(){
         $('.'+car_id).click(function () {
             var model_id=this.id;
             $.ajax({
-                url:"https://api.mlab.com/api/1/databases/car_models/collections/cars?apiKey=",
+                url:"https://api.mlab.com/api/1/databases/car_models/collections/cars?apiKey=nLpPIkExIzD45P9OyqCYrLS8JVtpBHlS",
                 type:"GET",
                 data:{ get_param: 'value' },
                 dataType:'json',
-                success:function(data,responseTxt, statusTxt, xhr){
+                success:function(data,responseText, statusTxt, xhr){
                     if(statusTxt == "success")
                         var parsedData =$.parseJSON(data)
+                    // $('.'+car_id).on("click",function(){
+                        $("."+car_id).fadeOut(1000)
+                        $('#final-message').fadeIn(2000,function () {
+                            $('#final-message').append( document.createTextNode( "Your car is a "+car_id+" "+model_id+" "+data[0]["make"][car_id]["models"][model_id]["category"] ) )
+
+                        })
+
+                    // })
                         console.log(data[0]["make"][car_id]["models"][model_id]["category"])
 
                     if(statusTxt == "error")
